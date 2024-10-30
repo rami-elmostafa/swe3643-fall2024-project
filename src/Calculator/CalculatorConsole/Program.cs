e4b759e2741602e3f9f6ca226c7c3548509eebb7
rami-elmostafa <relmost1@students.kennesaw.edu>
2024-10-30 13:04:20 -0400

    Trying to run it

---
+++ src/Calculator/CalculatorConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using CalculatorLogic;
+
+namespace CalculatorConsole;
+
+public class Program
+{
+public static void Main(string[] args)
+{
+    double userValue = 11.5;
+    double mean = 7;
+    double standardDeviation = 1.5811388300841898;
+    var ZScore = DescriptiveStatistics.ComputeZScore(userValue, mean, standardDeviation);
+    System.Console.WriteLine($"ZScore = {ZScore}");
+}
+}
